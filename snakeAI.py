58f6ad0bbcbce8f20e0b2180278a0ce1c9d5643a
Robin Andersson <robin182andersson@gmail.com>
2018-03-04 13:00:53 +0100

    Added a simple and stupid AI

--- snakeAI.py
+++ snakeAI.py
@@ -15,7 +15,7 @@ class Apple:
     def draw(self, surface, image):
         surface.blit(image, (self.x, self.y))
 
-class Player:
+class Computer:
     x = [0]
     y = [0]
     step = 44
@@ -31,15 +31,15 @@ class Player:
             self.x.append(-100)
             self.y.append(-100)
 
-        # initial positions, no collision.
+        #initial positions, no collision
         self.x[1] = 1*44
-        self.x[2] = 2*44
+        self.y[2] = 2*44
 
     def update(self):
         self.update_count += 1
         if (self.update_count > self.update_count_max):
             # update previous positions
-            for i in range(self.length-1,0,-1):
+            for i in range(self.length-1, 0, -1):
                 self.x[i] = self.x[i-1]
                 self.y[i] = self.y[i-1]
 
@@ -67,6 +67,17 @@ class Player:
     def moveDown(self):
         self.direction = 3
 
+    def target(self, dx, dy):
+        if (self.x[0] > dx):
+            self.moveLeft()
+        if (self.x[0] < dx):
+            self.moveRight()
+        if (self.x[0] == dx):
+            if (self.y[0] < dy):
+                self.moveDown()
+            if (self.y[0] > dy):
+                self.moveUp()
+
     def draw(self, surface, image):
         for i in range(0, self.length):
             surface.blit(image, (self.x[i], self.y[i]))
@@ -81,7 +92,6 @@ class Game:
 class App:
     window_width = 800
     window_height = 600
-    player = 0
     apple = 0
 
     def __init__(self):
@@ -90,8 +100,8 @@ class App:
         self._image_surf = None
         self._apple_surf = None
         self.game = Game()
-        self.player = Player(3)
         self.apple = Apple(5, 5)
+        self.computer = Computer(3)
 
     def on_init(self):
         pygame.init()
@@ -106,35 +116,35 @@ class App:
             self._running = False
 
     def on_loop(self):
-        self.player.update()
+        self.computer.target(self.apple.x, self.apple.y)
+        self.computer.update()
 
         # does snake eat apple?
-        for i in range(0, self.player.length):
-            if (self.game.isCollision(self.apple.x, self.apple.y, self.player.x[i], self.player.y[i], 44)):
-                self.apple.x = randint(2, 9) * 44
-                self.apple.y = randint(2, 9) * 44
-                self.player.length += 1
+        for i in range(0,self.computer.length):
+            if self.game.isCollision(self.apple.x, self.apple.y, self.computer.x[i], self.computer.y[i], 44):
+                self.apple.x = randint(2,9) * 44
+                self.apple.y = randint(2,9) * 44
+                self.computer.length += 1
                 
-        #does snake collide with itself?
-        for i in range(2, self.player.length):
-            if (self.game.isCollision(self.player.x[0], self.player.y[0], self.player.x[i], self.player.y[i], 40)):
+        # does snake collide with itself?
+        for i in range(2, self.computer.length):
+            if (self.game.isCollision(self.computer.x[0], self.computer.y[0], self.computer.x[i], self.computer.y[i], 40)):
                 print("You lose! Collision: ")
-                print("x[0] (" + str(self.player.x[0]) + "," + str(self.player.y[0]) + ")")
-                print("x[" + str(i) + "] (" + str(self.player.x[i]) + "," + str(self.player.y[i]) + ")")
+                print("x[0] (" + str(self.computer.x[0]) + "," + str(self.computer.y[0]) + ")")
+                print("x[" + str(i) + "] (" + str(self.computer.x[i]) + "," + str(self.computer.y[i]) + ")")
                 exit(0)
 
         # does snake collide with wall?
-        if (self.player.x[0] >= 800 or self.player.x[0] < 0 or self.player.y[0] >= 600 or self.player.y[0] < 0):
+        if (self.computer.x[0] >= 800 or self.computer.x[0] < 0 or self.computer.y[0] >= 600 or self.computer.y[0] < 0):
             print("You lose! Collision with wall!")
-            print(str(self.player.x[0]) + "," + str(self.player.y[0]) + ")")
+            print("x[0] (" + str(self.computer.x[0]) + "," + str(self.computer.y[0]) + ")")
             exit(0)
-
         pass
 
     def on_render(self):
         self._display_surf.fill((0, 0, 0))
-        self.player.draw(self._display_surf, self._image_surf)
         self.apple.draw(self._display_surf, self._apple_surf)
+        self.computer.draw(self._display_surf, self._image_surf)
         pygame.display.flip()
 
     def on_cleanup(self):
@@ -147,18 +157,6 @@ class App:
         while (self._running):
             pygame.event.pump()
             keys = pygame.key.get_pressed()
-        
-            if (keys[K_RIGHT]):
-                self.player.moveRight()
-            
-            if (keys[K_LEFT]):
-                self.player.moveLeft()
-            
-            if (keys[K_UP]):
-                self.player.moveUp()
-
-            if (keys[K_DOWN]):
-                self.player.moveDown()
             
             if (keys[K_ESCAPE]):
                 self._running = False
@@ -168,7 +166,6 @@ class App:
 
             time.sleep(1.0/30.0)
         self.on_cleanup()
-
 
 if __name__ == "__main__":
     theApp = App()
